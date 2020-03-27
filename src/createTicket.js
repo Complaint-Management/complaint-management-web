dbc2ba9476bdbf0d28675d5b60e1ed25437b9884
Megha <1605287@kiit.ac.in>
2020-03-28 01:09:30 +0530

    Form_added

--- src/createTicket.js
+++ src/createTicket.js
@@ -2,11 +2,17 @@ import React,{Component} from 'react';
 import './App.css';
 import Header from './Header'
 import Footer from './Footer'
+import CreateTicketForm from './CreateTicketForm'
 class createTicket extends Component {
     render(){
         return (
             <div>
                 <Header/>
+                <div>
+                    <h1 className="heading">Complaint Management System !</h1>
+                </div>
+                <CreateTicketForm/>
+                <Footer/>
             </div>
         )
     }
